59124694cce5e21b2cbe5b05aa31e072141e9f0d
Dhomênica Monteiro <dhomimonteiro@gmail.com>
2023-04-12 16:48:03 -0300

    Estilização cabeçalho pronta
    Terminada a estilização do cabeçalho. Falta adicionar a comunicação com o banco de dados

---
+++ cabecalho.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="home.php">
+            <img src="img/logo projeto - amarelo neon.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="menu-hamburg">
+                <i class="fa-solid fa-bars fa-xl"></i>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Freelancers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contratantes</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> -->
+                <li class="nav-item ola-usuario">
+                    <div class="row d-flex flex-column">
+                        <p class="ola">Olá,</p>
+                        <a href="" style="text-decoration: none;">
+                            <p class="usuario">USUÁRIO</p>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+            <!-- <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+        </div>
+    </div>
+</nav>
