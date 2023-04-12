9e7a72d45962341efbb23f7d3212c81da253a65b
Br-Moura <116111491+Br-Moura@users.noreply.github.com>
2023-04-12 17:20:32 -0300

    terminar de mexer login

---
+++ Projeto/Tela Login teste/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="">
+    <div class="containerLogin">
+        <div class="bgBranco">
+            <div class="caixa entrar">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h2>Já está cadastrado?</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <button class="btnEntrar">Entrar</button>
+                    </div>
+                </div>
+            </div>
+            <div class="caixa inscrever">
+                <h2>Não possui conta??</h2>
+                <button class="btnCadastrar">Cadastrar</button>
+            </div>
+        </div>
+        <div class="caixaForm">
+            <div class="form formEntrar">
+                <form>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3 class="h3Entrar">Entrar agora</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control" placeholder="Usuário">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <input type="password" class="form-control" placeholder="Senha">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <input type="submit" class="form-control" placeholder="Login">
+                        </div>
+                    </div>
+                    <a href="#" class="esqueceu">Esqueci minha senha</a>
+                </form>
+            </div>
+            <div class="form formInscrever">
+                <form>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3 class="h3Inscrever">Torne-se um freelancer</h3>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-sm-12">
+                            <button class=" btn-lg">Cadastrar como colaborador</button>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-sm-12">
+                            <h3 class="h3Inscrever">Torne-se um colaborador</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <button class="button form-control">Click Here</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ Projeto/Tela Login teste/script.js
@@ -0,0 +1,14 @@
+const caixaForm = document.querySelector(".caixaForm");
+const btnEntrar = document.querySelector(".btnEntrar");
+const btnCadastrar = document.querySelector(".btnCadastrar");
+const body = document.querySelector("body");
+
+btnCadastrar.onclick = function() {
+    caixaForm.classList.add('active');
+    body.classList.add('active');
+}
+
+btnEntrar.onclick = function() {
+    caixaForm.classList.remove('active');
+    body.classList.remove('active');
+}
