9201f5a6135c82d6aba60469da8956a8b603354d
Dhomênica Monteiro <dhomimonteiro@gmail.com>
2023-04-19 13:40:19 -0300

    form_CadastroFreelancer

---
+++ form_CadastroFreelancer.php
@@ -0,0 +1,266 @@
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="style.css">
+
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-sm-12 d-flex justify-content-center">
+            <h1>Cadastre-se a GetDev como Freelancer</h1>
+        </div>
+
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-lg-2 col-sm-0"></div>
+        <div class="col-lg-8 col-md-12">
+            <p>Itens com <span class="importante">*</span> são obrigatórios.</p>
+            <form action="" class="form-control">
+                <div class="row">
+                    <div class="col-lg-12 mt-2">
+                        <label for="txtNome" class="mt-2">Nome <span class="importante">*</span></label>
+                        <input type="text" id="txtNome" name="txtNome" placeholder="Insira seu nome completo" class="form-control input">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 mt-2">
+                        <label for="txtNome" class="mt-2">E-mail <span class="importante">*</span></label>
+                        <input type="email" id="txtEmail" name="txtEmail" placeholder="Insira seu e-mail" class="form-control input">
+                    </div>
+                    <div class="col-sm-6 mt-2">
+                        <label for="txtNome" class="mt-2">Login <span class="importante">*</span></label>
+                        <input type="text" id="txtLogin" name="txtLogin" placeholder="Insira seu login" class="form-control input">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 mt-2">
+                        <label for="txtNome" class="mt-2">Senha <span class="importante">*</span></label>
+                        <input type="text" id="txtSenha" name="txtSenha" placeholder="Insira seu nome completo" class="form-control input">
+                    </div>
+                    <div class="col-sm-6 mt-2">
+                        <label for="txtNome" class="mt-2">Confirmação de senha <span class="importante">*</span></label>
+                        <input type="text" id="txtConfirmarSenha" name="txtConfirmarSenha" placeholder="Insira seu nome completo" class="form-control input">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">CPF <span class="importante">*</span></label>
+                        <input type="text" id="txtCPF" name="txtCPF" placeholder="Insira seu CPF" class="form-control input">
+                    </div>
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">RG <span class="importante">*</span></label>
+                        <input type="text" id="txtRG" name="txtRG" placeholder="Insira seu RG" class="form-control input">
+                    </div>
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Data de nascimento <span class="importante">*</span></label>
+                        <input type="date" id="txtNascimento" name="txtNascimento" placeholder="Insira sua data de nascimento" class="form-control input">
+                    </div>
+                </div>
+
+                <!--CONTATO-->
+                <div class="row">
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Celular 1 <span class="importante">*</span></label>
+                        <input type="text" id="txtCelular1" name="txtCelular1" placeholder="Insira seu número de celular" class="form-control input txtCelular1" maxlength="14">
+                    </div>
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Celular 2</label>
+                        <input type="text" id="txtCelular2" name="txtCelular2" placeholder="Insira seu número de celular" class="form-control input txtCelular2">
+                    </div>
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Telefone</label>
+                        <input type="text" id="txtTelefone" name="txtTelefone" placeholder="Insira seu número de celular" class="form-control input txtTelefone">
+                    </div>
+                </div>
+
+                <!-- LOCALIZAÇÃO -->
+                <div class="row">
+                    <div class="col-sm-2 mt-2">
+                        <label for="txtNome" class="mt-2">Rua <span class="importante">*</span></label>
+                        <input type="text" id="txtRua" name="txtRua" placeholder="Insira sua rua." class="form-control input">
+                    </div>
+                    <div class="col-sm-2 mt-2">
+                        <label for="txtNome" class="mt-2">Número <span class="importante">*</span></label>
+                        <input type="text" id="txtNumero" name="txtNumero" class="form-control input ">
+                    </div>
+                    <div class="col-sm-2 mt-2">
+                        <label for="txtCEP" class="mt-2">CEP <span class="importante">*</span></label>
+                        <input type="text" id="txtCEP" name="txtCEP" placeholder="Insira o CEP." class="form-control input txtCEP">
+                    </div>
+                    <div class="col-sm-2 mt-2">
+                        <label for="txtNome" class="mt-2">Bairro <span class="importante">*</span></label>
+                        <input type="text" id="txtBairro" name="txtBairro" placeholder="Insira o bairro." class="form-control input">
+                    </div>
+                    <div class="col-sm-3 mt-2">
+                        <label for="txtNome" class="mt-2">Cidade <span class="importante">*</span></label>
+                        <input type="text" id="txtCidade" name="txtCidade" placeholder="Insira a cidade." class="form-control input">
+                    </div>
+                    <div class="col-sm-1 mt-2">
+                        <label for="txtNome" class="mt-2">UF <span class="importante">*</span></label>
+                        <input type="text" id="txtUF" name="txtUF" class="form-control input">
+                    </div>
+                </div>
+
+                <!--INFORMAÇÃO BANCÁRIA-->
+                <div class="row">
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Banco <span class="importante">*</span></label>
+                        <input type="text" id="txtBanco" name="txtBanco" class="form-control input">
+                    </div>
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Agência Bancária <span class="importante">*</span></label>
+                        <input type="text" id="txtUF" name="txtUF" class="form-control input">
+                    </div>
+                    <div class="col-sm-4 mt-2">
+                        <label for="txtNome" class="mt-2">Conta corrente <span class="importante">*</span></label>
+                        <input type="text" id="txtUF" name="txtUF" class="form-control input">
+                    </div>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+include_once('rodape.php')
+?>
+
+<script src="js/jquery-3.6.4.js"></script>
+<script type="text/javascript">
+    function mask(o, f) {
+        v_obj = o
+        v_fun = f
+        setTimeout("execmask()", 1)
+    }
+
+    function execmask() {
+        v_obj.value = v_fun(v_obj.value)
+    }
+
+    function masktel(v) {
+        //IMPEDE QUE O USUÁRIO COLOQUE PONTUAÇÃO
+        v = v.replace(/\D/g, "");
+        v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
+        v = v.replace(/(\d)(\d{4})$/, "$1-$2");
+        return v;
+
+    }
+
+    function maskcpf(v) {
+        v = v.replace(/\D/g, "");
+        v = v.replace(/(\d{3})(\d)/, "$1.$2");
+        v = v.replace(/(\d{3})(\d)/, "$1.$2");
+        v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+        return v;
+    }
+
+    function maskrg(v) {
+        v = v.replace(/\D/g, "");
+        v = v.replace(/(\d{2})(\d)/, "$1.$2");
+        v = v.replace(/(\d{3})(\d)/, "$1.$2");
+        v = v.replace(/(\d{2})(\d{1})$/, "$1-$2");
+        return v;
+    }
+
+    function maskcep(v) {
+        v = v.replace(/\D/g, "");
+        v = v.replace(/(\d{5})(\d)/, "$1-$2");
+        return v;
+    }
+
+
+    function maskcnpj(v) {
+        v = v.replace(/\D/g, "");
+        v = v.replace(/^(\d{2})(\d)/, "$1.$2");
+        v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+        v = v.replace(/\.(\d{3})(\d)/, ".$1/$2");
+        v = v.replace(/(\d{4})(\d)/, "$1-$2");
+        return v;
+
+    }
+
+    function idcss(el) {
+        return document.getElementById(el);
+    }
+
+    window.onload = function() {
+
+        //TEL FIXO -------
+        idcss('txtTelefone').setAttribute('maxlength', 14);
+        idcss('txtTelefone').onkeypress = function() {
+            mask(this, masktel);
+        }
+
+        //-------------
+
+        //CELULAR -------
+        idcss('txtCelular1').setAttribute('maxlength', 15);
+        idcss('txtCelular1').onkeypress = function() {
+            mask(this, masktel);
+        }
+
+        idcss('txtCelular2').setAttribute('maxlength', 15);
+        idcss('txtCelular2').onkeypress = function() {
+            mask(this, masktel);
+        }
+        //-------------
+
+        //CPF ---------
+
+        idcss('txtCPF').setAttribute('maxlength', 14);
+        idcss('txtCPF').onkeypress = function() {
+            mask(this, maskcpf);
+        }
+
+        //-------------
+
+        //RG ---------
+        idcss('txtRG').setAttribute('maxlength', 12);
+        idcss('txtRG').onkeypress = function() {
+            mask(this, maskrg);
+        }
+
+        //-------------
+
+        //CEP ---------
+        idcss('txtCEP').setAttribute('maxlength', 9);
+
+        idcss('txtCEP').onkeypress = function() {
+            mask(this, maskcep);
+        }
+        //-------------
+    }
+
+    $(document).ready(function() {
+        $('#txtCEP').blur(function() {
+            var cep = $(this).val().replace(/\D/g, '');
+            if (cep != "") {
+                var validacep = /^[0-9]{8}$/;
+
+                if (validacep.test(cep)) {
+                    $('#txtRua').val('...');
+                    $("#txtBairro").val('...');
+                    $("#txtCidade").val('...');
+                    $("#txtUF").val('...');
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function(dados) {
+
+                        if (!("erro" in dados)) {
+                            //Atualiza os campos com os valores da consulta.
+                            $("#txtRua").val(dados.logradouro);
+                            $("#txtBairro").val(dados.bairro);
+                            $("#txtCidade").val(dados.localidade);
+                            $("#txtUF").val(dados.uf);
+                        } //end if.
+                        else {
+                            //CEP pesquisado não foi encontrado.
+                            limpa_formulário_cep();
+                            alert("CEP não encontrado.");
+                        }
+                    });
+
+                }
+            }
+        })
+    })
+</script>
