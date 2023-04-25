c2040826682eebe0d37531e775aaab7edd70e14a
Br-Moura <116111491+Br-Moura@users.noreply.github.com>
2023-04-25 17:20:47 -0300

    oi

---
+++ perfil_contratante.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Document</title>
+</head>
+<body>
+    <?php include_once('cabecalho.php') ?>
+    <style>
+.background{
+    height: 200px;
+    width: 100%;
+    background: #FEBA1A;
+}
+.linha{
+    width: 100%;
+    height: 20px;
+}
+.img{
+    height: 200px;
+    width: 100%;
+    text-align: center;
+    background: black;
+    border-radius: 50%;
+}
+
+
+    </style>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="background"></div>
+            <div class="linha"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <div class="img">
+                </div>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </div>
+
+
+    
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-3.6.4.js"></script>
+    <script src="Projeto/TelaLoginteste/script.js"></script>
+</body>
+</html>
